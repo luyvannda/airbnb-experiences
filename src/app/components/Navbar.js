9d99b3bfe6b8f8bf90bc94016dddfefcdd594c7c
luyvannda <philips.joe329@gmail.com>
2023-10-09 15:59:11 +0700

    add logo to the project successfully

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,8 +2,8 @@ import logo from "./images/logo.png"
 
 export default function Navbar() {
   return (
-    <div className="nav">
-      <img src={logo} alt="a red airbnb logo"></img>
+    <div className="navbar">
+      <img className="logo" src={logo} alt="a red airbnb logo"></img>
     </div>
   )
 }
