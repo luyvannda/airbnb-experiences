e5e53bd9ede8547b5cc9a3a2ace7ecc46927b96b
luyvannda <philips.joe329@gmail.com>
2023-10-10 10:36:32 +0700

    add className to Hero elements and wrap it in <section> instead of <div>

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,10 +3,10 @@ import image from "./images/hero-background-image.png"
 
 export default function Hero() {
   return (
-    <div className="hero">
-      <img src={image} alt="group photos"></img>
-      <h1>Online Experiences</h1>
-      <p>Join unique interactive activities led by one-of-a-kind hosts&mdash;all without leaving home.</p>
-    </div>
+    <section className="hero">
+      <img className="hero-photo" src={image} alt="group photos"></img>
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts&mdash;all without leaving home.</p>
+    </section>
   )
 }
