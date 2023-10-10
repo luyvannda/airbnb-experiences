3f0d6c42150cb89deb7d2e7837f57a4fb96d0295
luyvannda <philips.joe329@gmail.com>
2023-10-10 15:00:07 +0700

    add status text to the first card image and add it text contents

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,15 +1,17 @@
 import photo1 from "./images/katie-swimmer.png"
 import star from "./images/Star.png"
+import ellipse from "./images/Ellipse.png"
 
 export default function Card() {
   return (
     <section className="card">
       <div className="first-card">
-        <img src={photo1} alt="A woman swimming instructor"></img>
-        <p>
-          <img src={star} alt="A small red star"></img>
-          5.0 <span>(6)  USA</span>
+        <p className="card1-status">SOLD OUT</p>
+        <img className="card-photo" src={photo1} alt="A woman swimming instructor"></img>
+        <p className="card-rating">
+          <img className="star" src={star} alt="A small red star"></img> 5.0 <span>(6) <img className="ellipse" src={ellipse} alt="an ellipse"></img> USA</span>
         </p>
+        <p className="card-description"></p>
       </div>
     </section>
   )
