b19350f30e0f4ecbd04cb7d19ee4d7746e95e14c
luyvannda <philips.joe329@gmail.com>
2023-10-13 11:01:41 +0700

    passing props to our Card component instead of hard coding it to make the Card component reusable

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,8 @@ import './App.scss';
 import Card from './components/Card';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import katie from "./components/images/katie-swimmer.png"
+
 
 function App() {
   return (
@@ -10,8 +12,22 @@ function App() {
       <header className="App-header">
         <Navbar />
         <Hero />
-        <Card />
       </header>
+      <main className='App-main'>
+        <Card
+          status="SOLD OUT"
+          img={katie}
+          alt="A woman swimming instructor"
+          rating={5.0}
+          reviewCount="(6)"
+          country="USA"
+          description="Life Lessons with Katie Zaferes"
+          price={136}
+
+        />
+        <Card />
+        <Card />
+      </main>
     </div>
   );
 }
--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,17 +1,30 @@
-import photo1 from "./images/katie-swimmer.png"
 import star from "./images/Star.png"
 import ellipse from "./images/Ellipse.png"
 
-export default function Card() {
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
+
+export default function Card(props) {
   return (
     <section className="card">
       <div className="first-card">
-        <p className="card1-status">SOLD OUT</p>
-        <img className="card-photo" src={photo1} alt="A woman swimming instructor"></img>
+        <p className="card1-status">{props.status}</p>
+        <img className="card-photo" src={props.img} alt={props.alt}></img>
         <p className="card-rating">
-          <img className="star" src={star} alt="A small red star"></img> 5.0 <span>(6) <img className="ellipse" src={ellipse} alt="an ellipse"></img> USA</span>
+          <img className="star" src={star} alt="A small red star"></img> {props.rating} <span>{props.reviewCount} <img className="ellipse" src={ellipse} alt="an ellipse"></img> {props.country}</span>
         </p>
-        <p className="card-description">Life lessons with Katie Zaferes <span>From $136</span> / person
+        <p className="card-description">{props.description} <span>From ${props.price}</span> / person
         </p>
       </div>
     </section>
