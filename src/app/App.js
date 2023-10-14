02ebd13c7b57d2b85a51396967726db25f2f72b3
luyvannda <philips.joe329@gmail.com>
2023-10-14 15:42:42 +0700

    use map() array method to turn data into props and display them

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,35 @@ import './App.scss';
 import Card from './components/Card';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import dataBase from './components/data';
 
+
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
 
 function App() {
+  const dataElements = dataBase.map((data) => {
+    return <Card
+      status={data.openSpots}
+      img={`./images/${data.coverImg}`}
+      alt={data.alt}
+      rating={data.stats.rating}
+      reviewCount={data.stats.reviewCount}
+      country={data.country}
+      title={data.title}
+      price={data.price}
+    />
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,22 +39,24 @@ function App() {
         <Hero />
       </header>
       <main className='App-main'>
-        <Card
-          status="SOLD OUT"
-          img="./images/katie-swimmer.png"
-          alt="A woman swimming instructor"
-          rating={5.0}
-          reviewCount="(6)"
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-
-        />
-        <Card />
-        <Card />
+        {dataElements}
       </main>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+<Card
+  status="SOLD OUT"
+  img="./images/katie-swimmer.png"
+  alt="A woman swimming instructor"
+  rating={5.0}
+  reviewCount="(6)"
+  country="USA"
+  title="Life Lessons with Katie Zaferes"
+  price={136}
+/>
+*/
--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -8,7 +8,7 @@ export default function Card(props) {
         <p className="card1-status">{props.status}</p>
         <img className="card-photo" src={props.img} alt={props.alt}></img>
         <p className="card-rating">
-          <img className="star" src={star} alt="A small red star"></img> {props.rating} <span>{props.reviewCount} <img className="ellipse" src={ellipse} alt="an ellipse"></img> {props.country}</span>
+          <img className="star" src={star} alt="A small red star"></img> {props.rating} <span>({props.reviewCount}) <img className="ellipse" src={ellipse} alt="an ellipse"></img> {props.country}</span>
         </p>
         <p className="card-description">{props.title} <span>From ${props.price}</span> / person
         </p>
